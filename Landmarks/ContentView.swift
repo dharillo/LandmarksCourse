7b75c3e26af7a1a80cbdd98aeacd7d87a4b61d8a
David Harillo Sánchez <dhs.seck@gmail.com>
2021-11-20 17:50:05 +0100

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by David Harillo Sánchez on 20/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
