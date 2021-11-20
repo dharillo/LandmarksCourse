4a986083c9a653d73dd67d6072caf010ef7d0058
David Harillo Sánchez <dhs.seck@gmail.com>
2021-11-20 18:57:43 +0100

    Finish section 5 of the tutorial where a map view is added to the project

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,25 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by David Harillo Sánchez on 20/11/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 33.860_153, longitude: -115.842_590),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
